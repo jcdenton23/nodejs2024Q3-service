18fe713b42d808f350f3ef789896fb20230da7ee
Ali <alisher_tashbulatov@epam.com>
2024-11-09 23:36:11 +0500

    feat: implement doc

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as dotenv from 'dotenv';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 dotenv.config();
 
@@ -14,6 +15,14 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  const config = new DocumentBuilder()
+    .setTitle('Home Library API')
+    .setDescription('The home-library API')
+    .setVersion('1.0')
+    .addTag('home-library')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, documentFactory);
   await app.listen(process.env.PORT);
 }
 bootstrap();
