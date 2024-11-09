3ff57c05dd88fa724be2d8efe4394951ad2f37b7
Ali <alisher_tashbulatov@epam.com>
2024-11-09 22:43:15 +0500

    feat: add .env file

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +14,6 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  await app.listen(4000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
